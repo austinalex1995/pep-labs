3a46a0d694d372681c8f0bfc14a9c969c4c78e8a
Austin Alex <aealex1995@gmail.com>
2022-12-27 20:26:06 +0000

    Saved progress on Instantiation 2022-12-27 20:26:06 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String s = "string";
+        return s;
     }
 }
