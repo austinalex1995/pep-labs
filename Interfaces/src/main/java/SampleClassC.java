07d8c764972093d06637457ebeda7e26cdcd02d8
Austin Alex <aealex1995@gmail.com>
2023-01-25 01:44:19 +0000

    Saved progress on Interfaces 2023-01-25 01:44:19 +0000

--- Interfaces/src/main/java/SampleClassC.java
+++ Interfaces/src/main/java/SampleClassC.java
@@ -27,6 +27,11 @@ public class SampleClassC implements Comparable<SampleClassC>{
      * @param other another object of type SampleClassC, which we are comparing to 'this' object.
      */
     public int compareTo(SampleClassC other) {
+        if (val > other.val) {
+            return 1;
+        } else if (val < other.val) {
+            return -1;
+        }
         return 0;
     }
 
