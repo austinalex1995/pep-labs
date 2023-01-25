7f42c1e32eae9711a913873fd1faeb62a2574ba4
Austin Alex <aealex1995@gmail.com>
2023-01-25 18:52:06 +0000

    Saved progress on MostCommonCharacter 2023-01-25 18:52:06 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -9,7 +9,16 @@ public class LargestSum {
      * @param nums a list of ints.
      * @return the largest possible sum of separate numbers from nums.
      */
-    public int bigSum(List<Integer> nums){
-        return 0;
+    public int bigSum(List<Integer> nums) {
+        int a = -1;
+        int b = -1;
+        for(int i = 0; i < nums.size(); i++) {
+            if (a < nums.get(i)) {
+                a = nums.get(i);
+            } else if (b < nums.get(i)) {
+                b = nums.get(i);
+            }
+        }
+        return (a+b);
     }
 }
