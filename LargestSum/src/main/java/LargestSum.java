97f00d651c3195f46693e9e4db52059378395d55
Austin Alex <aealex1995@gmail.com>
2023-01-25 20:13:42 +0000

    Saved progress on LargestSum 2023-01-25 20:13:42 +0000

--- LargestSum/src/main/java/LargestSum.java
+++ LargestSum/src/main/java/LargestSum.java
@@ -13,10 +13,24 @@ public class LargestSum {
         int a = -1;
         int b = -1;
         for(int i = 0; i < nums.size(); i++) {
-            if (a < nums.get(i)) {
+            if (a == -1) {
                 a = nums.get(i);
-            } else if (b < nums.get(i)) {
+            } else if (b == -1) {
                 b = nums.get(i);
+            } else {
+                int aDifference = (a - nums.get(i));
+                int bDifference = (b - nums.get(i));
+                if (aDifference < 0 && bDifference < 0) {
+                    if (aDifference < bDifference) {
+                        a = nums.get(i);
+                    } else {
+                        b = nums.get(i);
+                    }
+                } else if (aDifference < 0) {
+                    a = nums.get(i);
+                } else if (bDifference < 0) {
+                    b = nums.get(i);
+                }
             }
         }
         return (a+b);
